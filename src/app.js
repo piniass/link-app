ed57b6bc8a25f3b4333163125c8cc04dcf4c0940
miguel <miguelpi1998@gmail.com>
2024-07-16 21:24:04 +0200

    cors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import cors from 'cors'
 const app = express()
 
 app.use(cors({
-    origin: 'https://linkeados.vercel.app/',
+    origin: ['https://linkeados.vercel.app/', 'http://localhost:5173'],
     credentials: true
 }))
 app.use(morgan('dev'))
