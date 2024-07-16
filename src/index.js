42d63e08df2096b042bfcc9ac5c30d9ab40184fb
miguel <miguelpi1998@gmail.com>
2024-07-16 23:42:03 +0200

    vercel

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-/*import app from "./app.js";
-import { connectDB } from "./db.js";
 
-connectDB();
-
-app.listen(3000, () => {
-    console.log("Server on port 3000");
-});
-*/
 import app from "./app.js";
 import { connectDB } from "./db.js";
 
