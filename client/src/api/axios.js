229732989243cf1f2f19fbe4eec3f0905a4a6ed3
miguel <miguelpi1998@gmail.com>
2024-07-16 21:17:37 +0200

    .

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://linkeados-backend.vercel.app/api/',
     withCredentials: true
 })
 
