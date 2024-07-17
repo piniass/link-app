7d357ce04d775ab1f26e6b648e429be5f05f02c5
miguel <miguelpi1998@gmail.com>
2024-07-17 10:41:09 +0200

    puerto

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import { connectDB } from "./db.js";
 
 connectDB();
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
     console.log("Corriendo");
 });
 
