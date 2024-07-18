58e7956b92a2fd7e54eae575369039b15dc15ebd
miguel <miguelpi1998@gmail.com>
2024-07-18 19:40:20 +0200

    credentials

--- client/src/api/link.js
+++ client/src/api/link.js
@@ -1,7 +1,7 @@
 import axios from "./axios";
 
-export const getLinksRequest = () => axios.get('/link')
-export const getLinkRequest = () => axios.get(`/link/${link._id}`)
-export const createLinkRequest = (link) => axios.post('/link', link)
-export const updateLinkRequest = (link, id) => axios.put(`/link/${id}`, link)
-export const deleteLinkRequest = (id) => axios.delete(`/link/${id}`)
+export const getLinksRequest = () => axios.get('/link', { withCredentials: true })
+export const getLinkRequest = () => axios.get(`/link/${link._id}`, { withCredentials: true })
+export const createLinkRequest = (link) => axios.post('/link', link, { withCredentials: true })
+export const updateLinkRequest = (link, id) => axios.put(`/link/${id}`, link, { withCredentials: true })
+export const deleteLinkRequest = (id) => axios.delete(`/link/${id}`, { withCredentials: true })
