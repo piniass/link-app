0ab9d9903607eecc1efb6236c08bf25805a9e85b
miguel <miguelpi1998@gmail.com>
2024-07-18 19:24:58 +0200

    credentials

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,22 +1,31 @@
 import axios from "./axios";
 
-export const registerRequest = user => axios.post(`/register`, user);
-export const loginRequest = user => axios.post(`/login`, user);
-export const editRequest = user => axios.put(`/edit`, user);
-export const verifyTokenRequest = async () => axios.get(`/auth/verify`);
-export const getInfo = () => axios.get('/profile')
+import axios from 'axios';
+
+export const registerRequest = user => axios.post('/register', user, { withCredentials: true });
+
+export const loginRequest = user => axios.post('/login', user, { withCredentials: true });
+
+export const editRequest = user => axios.put('/edit', user, { withCredentials: true });
+
+export const verifyTokenRequest = async () => axios.get('/auth/verify', { withCredentials: true });
+
+export const getInfo = () => axios.get('/profile', { withCredentials: true });
+
 export const putImage = async (image) => {
-    try {
-      const formData = new FormData();
-      formData.append('image', image);
-  
-      const response = await axios.put('/image', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      });
-  
-    } catch (error) {
-      throw error; // Lanza el error para manejarlo en la función llamante
-    }
-  };
+  try {
+    const formData = new FormData();
+    formData.append('image', image);
+
+    const response = await axios.put('/image', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+      withCredentials: true,
+    });
+
+    return response; // Asegúrate de retornar la respuesta o manejarla según sea necesario
+  } catch (error) {
+    throw error; // Lanza el error para manejarlo en la función llamante
+  }
+};
