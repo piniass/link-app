54a574353dd4476485b4fdb35aa4488f6a19c579
miguel <miguelpi1998@gmail.com>
2024-07-23 14:12:31 +0200

    comentario

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 export default function ProtectedRoute() {
     const {loading, user, isAuthenticated} = useAuth()
-    console.log(loading, isAuthenticated)
+    // console.log(loading, isAuthenticated)
     if(!loading && !isAuthenticated) return <Navigate to='/' replace/>
   return (
     <Outlet/>
